e3337c2504d7b21c27fdf155e979e268585fde5c
charliee97 <carlosgcereceda@hotmail.es>
2018-03-16 03:39:01 +0100

    dsf

--- src/mutacion/MutacionEstandarReal.java
+++ src/mutacion/MutacionEstandarReal.java
@@ -1,14 +1,22 @@
 package mutacion;
 
-import genotipo.GenotipoBinario;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 import genotipo.GenotipoReal;
 
-public class MutacionEstandarReal<Fenotipo,Fitness> implements Mutacion<GenotipoReal,Fenotipo,Fitness> {
+public class MutacionEstandarReal<Fenotipo, Fitness> implements Mutacion<GenotipoReal, Fenotipo, Fitness> {
 
 	@Override
 	public void muta(GenotipoReal genotipo, double prob_mutacion) {
-		// TODO Auto-generated method stub
-		
+		for (int i = 0; i < genotipo.getNumGenes(); i++) {
+			Random r = new Random();
+			double random = r.nextDouble();
+			if (random < prob_mutacion) {
+
+			}
+		}
+
 	}
 
 }
